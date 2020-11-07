20fdf3b8f8f7037f3b867db01073cfbbc1ee308d
Ossamoon <osamu950623@gmail.com>
2020-11-07 22:44:12 +0900

    made LoginView

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def signupfunc(request):
-    user2 = User.objects.get(username = 'osamusaito')
-    print(user2.email)
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username = username2)
+            return render(request, 'signup.html', {'error' : 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some' : 100})
+    else:
         return render(request, 'signup.html', {'some' : 100})
 
-    return render(request, 'signup.html', {'some' : 100})
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password = password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
