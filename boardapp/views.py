706a9595ba61ed9bd5449e8069235fc066d90f20
Ossamoon <osamu950623@gmail.com>
2020-11-07 21:01:37 +0900

    made SignUpView

---
+++ boardapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def signupfunc(request):
+    user2 = User.objects.get(username = 'osamusaito')
+    print(user2.email)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        return render(request, 'signup.html', {'some' : 100})
+
+    return render(request, 'signup.html', {'some' : 100})
